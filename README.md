d390ef32189746c7d474fa31b5d22eba7dde3b8a
Souvlaki42 <77171255+Souvlaki42@users.noreply.github.com>
2024-01-20 17:00:39 +0200

    New typescript version

--- README.md
+++
@@ -1,3 +0,0 @@
-# YouTube Timestamp Saver
-
-Automatically save the current video timestamp as the t query param when pausing a youtube video.
---
+++ youtube.ts
@@ -0,0 +1,21 @@
+const handleTimestampSave = (player: HTMLVideoElement) => {
+	const execute = () => {
+		const url = new URL(window.location.href);
+		const rawTimestamp = player.currentTime;
+		const integerTimestamp = Math.floor(rawTimestamp);
+		const parsedTimestamp = integerTimestamp.toString();
+		url.searchParams.set("t", parsedTimestamp);
+		history.replaceState("", "", url.href);
+	};
+	player.addEventListener("pause", execute);
+	window.addEventListener("blur", execute);
+};
+try {
+	const videoPlayerQuery = "#movie_player > div.html5-video-container > video";
+	const videoPlayer: HTMLVideoElement | null =
+		document.querySelector(videoPlayerQuery);
+	if (!videoPlayer) throw new Error("No video player found in the page.");
+	handleTimestampSave(videoPlayer);
+} catch (error) {
+	console.info(error);
+}
